bf1c7a2d15117f63d967bf8e9644bef33ee5dfbc
Amarnathamraj <amarnath987654@gmail.com>
2025-05-27 19:44:32 +0530

    added code to anand branch

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.post('/home',(req,resp)=>{
     console.log('pos trequest')
     resp.send('<h1>post regwuest</h1>')
 })
+app.put('/home',(req,res)=>{
+    console.log('put requestr')
+    res.send({name:'amarnath',age:26})
+})
 app.listen(8080,()=>{
     console.log('app is listening at port no 8080')
 })
