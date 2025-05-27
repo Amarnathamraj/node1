ec44ce6e13aa401b86646d1ee59e5b794c168c18
Amarnathamraj <amarnath987654@gmail.com>
2025-05-27 19:57:31 +0530

    delted added by govind branch

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.post('/home',(req,resp)=>{
     console.log('pos trequest')
     resp.send('<h1>post regwuest</h1>')
 })
+app.delete('/home',(req,res)=>{
+    console.log('delted ')
+    res.send('deleted ')
+})
 app.listen(8080,()=>{
     console.log('app is listening at port no 8080')
 })
